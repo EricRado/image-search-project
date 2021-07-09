11b9bd2396f857458199e8d87a3116cdee3fb796
Eric-Rado <eric.rado@whoop.com>
2021-07-09 08:35:35 -0400

    Built basic UI for image search list screen

---
+++ ImageSearchProject/ImageSearchList/ImageCollectionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ImageCollectionViewController.swift
+//  ImageSearchProject
+//
+//  Created by Eric Rado on 7/9/21.
+//
+
+import UIKit
+
+final class ImageCollectionViewController: UIViewController {
+    // MARK: - UI properties
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        return searchController
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.keyboardDismissMode = .onDrag
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    // MARK: - View helper methods
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        // setup navigation bar
+        navigationItem.title = "Images"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+extension ImageCollectionViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
+}
