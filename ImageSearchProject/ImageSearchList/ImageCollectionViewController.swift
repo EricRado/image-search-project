d0514ee6d9234b5601e72a895e6082b27a325c06
Eric-Rado <eric.rado@whoop.com>
2021-07-10 10:25:42 -0400

    Creating layout for collection view cells, setting up data source and binding view models to a cell

--- ImageSearchProject/ImageSearchList/ImageCollectionViewController.swift
+++ ImageSearchProject/ImageSearchList/ImageCollectionViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 final class ImageCollectionViewController: UIViewController {
+    // MARK: - Enum
+    private enum Section {
+        case all
+    }
+    
     // MARK: - UI properties
     private lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
@@ -18,9 +23,7 @@ final class ImageCollectionViewController: UIViewController {
     }()
     
     private lazy var collectionView: UICollectionView = {
-        // TODO: - Build layout with compositional layout
-        let layout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
         collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.identifier)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.keyboardDismissMode = .onDrag
@@ -28,6 +31,10 @@ final class ImageCollectionViewController: UIViewController {
         return collectionView
     }()
     
+    private var dataSource: UICollectionViewDiffableDataSource<Section, ImageCellViewModel>?
+    private var snapshot: NSDiffableDataSourceSnapshot<Section, ImageCellViewModel>?
+    private var images: [Image] = []
+    private var imageCellViewModels: [ImageCellViewModel] = []
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +58,68 @@ final class ImageCollectionViewController: UIViewController {
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    // MARK: - UICollectionView helper methods
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(130))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 8, bottom: 8, trailing: 8)
+        
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.scrollDirection = .vertical
+        
+        return UICollectionViewCompositionalLayout(section: section, configuration: configuration)
+    }
+    
+    private func createDataSource() {
+        self.dataSource = UICollectionViewDiffableDataSource<Section, ImageCellViewModel>(
+            collectionView: collectionView,
+            cellProvider: { (collectionView, indexPath, viewModel) -> UICollectionViewCell in
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCell.identifier, for: indexPath) as? ImageCell else {
+                    return UICollectionViewCell()
+                }
+                
+                cell.viewModel = viewModel
+                return cell
+            })
+    }
+    
+    private func updateUI() {
+        guard let dataSource = dataSource else { return }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ImageCellViewModel>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(imageCellViewModels, toSection: .all)
+        
+        dataSource.apply(snapshot)
+        
+        self.snapshot = snapshot
+    }
+    
+    // MARK: - Network call methods
+    private func getImages(with text: String) {
+        imageRepo.fetchImages(with: text) { result in
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                switch result {
+                case .success(let images):
+                    let viewModels = images.map { ImageCellViewModel(with: $0) }
+                    self.images.append(contentsOf: images)
+                    self.imageCellViewModels.append(contentsOf: viewModels)
+                    self.createDataSource()
+                    self.updateUI()
+                case .failure(let error):
+                    // TODO: - Handle error
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
