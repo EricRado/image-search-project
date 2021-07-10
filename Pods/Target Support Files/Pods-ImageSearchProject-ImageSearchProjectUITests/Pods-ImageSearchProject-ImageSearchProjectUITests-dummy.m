f347ea95c57527f80a1bbf50d3b244b9f999e14f
Eric-Rado <eric.rado@whoop.com>
2021-07-10 12:47:03 -0400

    Add cocoapods and kingfisher for downloading images

---
+++ Pods/Target Support Files/Pods-ImageSearchProject-ImageSearchProjectUITests/Pods-ImageSearchProject-ImageSearchProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageSearchProject_ImageSearchProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ImageSearchProject_ImageSearchProjectUITests
+@end
---
+++ Pods/Target Support Files/Pods-ImageSearchProjectTests/Pods-ImageSearchProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageSearchProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_ImageSearchProjectTests
+@end
